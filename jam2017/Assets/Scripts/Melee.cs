3b0295f1474c70289b261e142f8c2959a5081bf3
Emile Fortin <sylfschiffer@gmail.com>
2017-02-24 18:34:49 -0500

    Création du squelette du système de personnage

---
+++ jam2017/Assets/Scripts/Melee.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Melee
+{
+    protected AudioClip sfx;
+    protected GameObject hitbox;
+
+    public Melee()
+    {
+
+    }
+}
+
+public class SuitCase : Melee {
+    
+    public SuitCase() : base() // Constructeur
+    {
+
+    }
+}
+
+public class Lightsaber : Melee
+{
+    public Lightsaber() : base() // Constructeur
+    {
+
+    }
+}
+
+public class Katana : Melee
+{
+    public Katana() : base() // Constructeur
+    {
+
+    }
+}
+
+public class PitchFork : Melee
+{
+    public PitchFork() : base() // Constructeur
+    {
+
+    }
+}
