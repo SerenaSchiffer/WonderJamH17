7707d27f1151e16be997421c656af6523707bf5e
Rafaël Côté <raf87crosby@hotmail.com>
2017-02-25 11:58:37 -0500

    fini juggernaut

--- jam2017/Assets/Scripts/MovableEnemy.cs
+++ jam2017/Assets/Scripts/MovableEnemy.cs
@@ -5,86 +5,144 @@ using UnityEngine;
 public enum TypeMoveableEnemy
 {
     Soldier,
-    Robot
+    Robot,
+    Juggernaut,
+    Sniper
 }
 
 public class MovableEnemy : MonoBehaviour {
 
     public TypeMoveableEnemy type;
+    public float nextShot;
+    public float spreadShoot;
 
     private GameObject destination;
     private GameObject playerToShoot;
     private Vector2 direction;
+    private SelectedSpawnPosition ssp;
 
-    private float nextShot = 0;
     private bool isNear = false;
     private bool isShooting = false;
-    private float spreadShootRobot = 20;
-    
-	// Use this for initialization
-	void Start () {
+    private float decNextShot = 0;
+
+    // Use this for initialization
+    void Start() {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        ReachDestination();
 
-        if (isShooting && nextShot == 0)
+    // Update is called once per frame
+    void Update() {
+        ReachDestination();
+        if (isShooting && decNextShot == 0)
+        {
+            decNextShot = nextShot;
             shootPlayer(playerToShoot);
-        else if (nextShot > 0)
-            nextShot -= Time.deltaTime;
-        else if (nextShot < 0)
-            nextShot = 0;
-	}
+        }
+        else if (decNextShot > 0)
+            decNextShot -= Time.deltaTime;
+        else if (decNextShot < 0)
+            decNextShot = 0;
+    }
 
     private void ReachDestination()
     {
         if (type == TypeMoveableEnemy.Soldier)
         {
             destination = GameObject.FindGameObjectWithTag("DestinationSoldier");
-            direction = GetDistance(destination);
-
-            if (direction.magnitude < 0.1f)
-            {
-                direction = Vector2.zero;
-                AddVelocity(0);
-            }
-            else
-            {
-                AddVelocity(100);
-            }
-                
+            GoToDestination();
         }
         else if (type == TypeMoveableEnemy.Robot)
         {
-            float minimalMagnitude;
-            float magnitudePlayerSoldier;
-            Vector2 distance;
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            destination = players[0];
+            FollowPlayer();
+        }
+        else if (type == TypeMoveableEnemy.Juggernaut || type == TypeMoveableEnemy.Sniper)
+        {
+            isShooting = true;
+            destination = GetSelectedSpawnPositionObject();
+            GoToDestination();
+            playerToShoot = GameObject.FindGameObjectWithTag("DestinationSoldier");
+        }
+        else if (type == TypeMoveableEnemy.Sniper)
+        {
+            isShooting = true;
+            destination = GetSelectedSpawnPositionObject();
+            GoToDestination();
+            //playerToShoot =
+        }
+    }
 
-            distance = GetDistance(destination);
-            minimalMagnitude = distance.magnitude;
+    private void GoToDestination()
+    {
+        //this.destination = destination;
+        direction = GetDistance(this.destination);
 
-            foreach (GameObject player in players)
+        if (direction.magnitude < 0.1f)
+        {
+            direction = Vector2.zero;
+            AddVelocity(0);
+        }
+        else
+        {
+            AddVelocity(100);
+        }
+    }
+
+    private void FollowPlayer()
+    {
+        float minimalMagnitude;
+        float magnitudePlayerSoldier;
+        Vector2 distance;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        destination = players[0];
+
+        distance = GetDistance(destination);
+        minimalMagnitude = distance.magnitude;
+
+        foreach (GameObject player in players)
+        {
+            distance = GetDistance(player);
+            magnitudePlayerSoldier = distance.magnitude;
+
+            if (minimalMagnitude > magnitudePlayerSoldier)
             {
-                distance = GetDistance(player);
-                magnitudePlayerSoldier = distance.magnitude;
-
-                if (minimalMagnitude > magnitudePlayerSoldier)
-                {
-                    minimalMagnitude = magnitudePlayerSoldier;
-                    destination = player;
-                }
-            }
-
-            if (!isNear)
-            {
-                direction = GetDistance(destination);
-                AddVelocity(80);
+                minimalMagnitude = magnitudePlayerSoldier;
+                destination = player;
             }
         }
+
+        if (!isNear)
+        {
+            direction = GetDistance(destination);
+            AddVelocity(80);
+        }
+    }
+
+    private GameObject GetSelectedSpawnPositionObject()
+    {
+        GameObject destination;
+
+        switch (ssp)
+        {
+            case SelectedSpawnPosition.Left:
+                destination = GameObject.Find("LeftDestination");
+                break;
+
+            case SelectedSpawnPosition.Right:
+                destination = GameObject.Find("RightDestination");
+                break;
+
+            default:
+                destination = GameObject.Find("BottomDestination");
+                break;
+        }
+
+        if (type == TypeMoveableEnemy.Sniper)
+        {
+            destination.transform.position += new Vector3(0, 3);
+            return destination;
+        }
+        else
+            return destination;
     }
 
     private void AddVelocity(float speed)
@@ -98,21 +156,16 @@ public class MovableEnemy : MonoBehaviour {
         Transform projectilePosition = gameObject.transform;
         GameObject bullet = (GameObject)Instantiate(Resources.Load("Prefabs/Bullet"));
 
-        if (type == TypeMoveableEnemy.Soldier)
-            nextShot = 0.75f;
-        else
-            nextShot = 0.35f;
-
         bullet.transform.position = transform.position;
         direction = GetDistance(other);
 
-        if (type == TypeMoveableEnemy.Robot)
+        if (type == TypeMoveableEnemy.Robot || type == TypeMoveableEnemy.Juggernaut)
         {
-            float random = Random.Range(-spreadShootRobot, spreadShootRobot);
+            float random = Random.Range(-spreadShoot, spreadShoot);
             direction = Quaternion.AngleAxis(random, Vector3.forward) * direction;
         }
 
-        bullet.GetComponent<Rigidbody2D>().velocity = direction * 5;
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * 1.5f;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -140,5 +193,10 @@ public class MovableEnemy : MonoBehaviour {
     Vector2 GetDistance(GameObject other)
     {
         return other.transform.position - gameObject.transform.position;
+    }
+
+    public void SetSelectedSpawnPosition(SelectedSpawnPosition ssp)
+    {
+        this.ssp = ssp;
     }
 }
