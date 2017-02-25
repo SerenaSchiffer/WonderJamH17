3d89e45127bf260e40073c895c7529bd12401cc3
Nicholas Fillion Lalancette <nicholala74@hotmail.com>
2017-02-24 20:28:34 -0500

    Viseur suit Right stick

--- jam2017/Assets/Scripts/PlayableChar.cs
+++ jam2017/Assets/Scripts/PlayableChar.cs
@@ -13,13 +13,26 @@ public class PlayableChar : MonoBehaviour
     protected Melee melWeapon;
     protected Guns rangWeapon;
 
+    int PlayerIdNumber;
+    //enum Controls {Attack = "R2" ,Melee = "R1" ,SpecialAttack = "L1" };
+
     // Useful Functions
     public virtual void Start(){ }
     protected virtual void SpecialAttack(){ }
 
     public void Update()
     {
+        float x = Input.GetAxisRaw("RightAxisXPlayer1");
+        float y = Input.GetAxisRaw("RightAxisYPlayer1");
+        Transform viseur = transform.GetChild(0);
+        viseur.position = new Vector2(x, y) * 2;
+
+        Debug.Log(x + " " + y);
         // Check for controls
+        /*if (Input.GetButtonDown("LeftAxisXPlayer"+PlayerIdNumber))
+        {
+            
+        }*/
     }
 
     protected virtual void UseSpecial()
