763dc188a37214774c75f604a1424590e774bf43
Pierre-André Savard <pandre711@gmail.com>
2017-02-24 19:49:41 -0500

    Correction du script playerChar et ajout des script pour chaque type de character

---
+++ jam2017/Assets/Scripts/PlayablesCharac/PlayableAnonymous.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableAnonymous : PlayableChar {
+
+    // Use this for initialization
+    public override void Start()
+    {
+        melWeapon = new Katana();
+        rangWeapon = new Uzi();
+    }
+
+    protected override void UseSpecial()
+    {
+        // Effects of the special attack
+    }
+
+}
