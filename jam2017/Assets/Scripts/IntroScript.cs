e911bb92e4388b3e9527157c4840f031de88af7c
Emile Fortin <sylfschiffer@gmail.com>
2017-02-24 23:33:40 -0500

    Avancement des intros et autres

---
+++ jam2017/Assets/Scripts/IntroScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class IntroScript : MonoBehaviour {
+
+    string story = "-- The  year  is  2048 -- \n Mankind  lives  under  an  absolute\n  totalitarian  regime  lead  by\n\n--  Supreme  Leader  Felix  Kjellberg --";
+
+    public Text textBox ;
+
+    public int aliveFrames;
+
+
+
+    void Start()
+    {
+        aliveFrames = 0;
+    }
+
+    void Update()
+    {
+        textBox.text = story.Substring(0, Mathf.Min(aliveFrames / 3, story.Length));
+        aliveFrames++;
+
+        if (aliveFrames == 441)
+        {
+            story = "Freedom  of  thought  is  a  thing  of  the  past\nHowever,  four  brave  rebels\nrise  up  to  free  mankind";
+            aliveFrames = 0;
+            StartCoroutine(LoadMain());
+        }
+    }
+
+    IEnumerator LoadMain()
+    {
+        yield return new WaitForSeconds(7f);
+        SceneManager.LoadScene("Title");
+    }
+}
