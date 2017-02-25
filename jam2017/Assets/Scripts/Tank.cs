c0249e17d3c12657122284cb70806370e088f04f
Pierre-André Savard <pandre711@gmail.com>
2017-02-25 12:41:02 -0500

    Tank fini

---
+++ jam2017/Assets/Scripts/Tank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+
+    Rigidbody2D tankBody;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+        tankBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tankBody.velocity = new Vector2(speed, 0);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
