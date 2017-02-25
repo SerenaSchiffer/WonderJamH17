dae239fcfcfdacd90de91fa119a317704d707de8
Emile Fortin <sylfschiffer@gmail.com>
2017-02-25 00:26:33 -0500

    Ajout d<un ecran de logo

---
+++ jam2017/Assets/Scripts/LogoScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(load_main_scene());
+	}
+
+    IEnumerator load_main_scene()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Intro");
+    }
+	
+}
