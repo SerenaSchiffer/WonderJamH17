8ea00c11419508998c6756e6c14686f2f654d47a
Nicholas Fillion Lalancette <nicholala74@hotmail.com>
2017-02-25 18:44:33 -0500

    Xwing Completed sauf damage

---
+++ jam2017/Assets/Scripts/XWingShadow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XWingShadow : MonoBehaviour {
+    Rigidbody2D tankBody;
+    public int speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        tankBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tankBody.velocity = transform.parent.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //fonction de kill
+            Destroy(other.gameObject);
+        }
+    }
+}
