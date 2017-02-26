222cf7a16c5e98864b1917c4dc2f57ca8d2ddd6d
Pierre-André Savard <pandre711@gmail.com>
2017-02-26 01:21:49 -0500

    Condition fonctionnel mais pas testé

--- jam2017/Assets/Scripts/WinCondition.cs
+++ jam2017/Assets/Scripts/WinCondition.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WinCondition : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public GameObject[] players;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        players = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log(players.Length);
         if(players.Length == 1)
         {
-
+            EndScript.SetWinPlayer(players[0].GetComponentInParent<PlayableChar>().PlayerIdNumber);
+            switch (players[0].name)
+            {
+                case "Rebel":
+                    EndScript.SetClassID(1);
+                    break;
+                case "Anonymous":
+                    EndScript.SetClassID(2);
+                    break;
+                case "BusinessMan":
+                    EndScript.SetClassID(3);
+                    break;
+                case "Farmer":
+                    EndScript.SetClassID(4);
+                    break;
+                default:
+                    break;
+            }
+            Application.LoadLevel("End_Player");
         }
 	}
 }
