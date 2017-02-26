6d8207b23ba86cc4edd218ab147ce2abb4602dfa
Nicholas Fillion Lalancette <nicholala74@hotmail.com>
2017-02-25 20:23:52 -0500

    Xwing spawn is now random

--- jam2017/Assets/Scripts/XWingAttack.cs
+++ jam2017/Assets/Scripts/XWingAttack.cs
@@ -6,8 +6,9 @@ public class XWingAttack : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        float random = (float)Random.Range(0f, 360f);
+        gameObject.transform.Rotate(0, 0, random);
+    }
 	
 	// Update is called once per frame
 	void Update () {
